a52f474e2d51eb96bfc0976f1a577fb657410474
Tri Vuong <tvuong@attinteractive.com>
2012-02-07 10:20:23 -0800

    update slides and css

--- public/javascript/master.js
+++ public/javascript/master.js
@@ -6,7 +6,10 @@ $(function() {
     keycode = event.keyCode;
     if ([37, 39].indexOf(keycode) == -1) return;
     
-    n = (keycode == 39) ? n + 1 : n - 1; 
+    n = (keycode == 39) ? n + 1 : n - 1;
+    
+    if (n < 1) n = 1;
+    if (n > slides.length) n = slides.length;
     
     slides.hide();
     $("#slide_" + n).show();
